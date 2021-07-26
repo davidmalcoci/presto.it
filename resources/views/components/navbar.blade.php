4cd39769e45598c366fe0daf06e6ec6289ea0c71
Agostino Toriello <torielloagostino@gmail.com>
2021-07-26 16:58:42 +0200

    homepage

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,3 @@
-        {{-- _____________________________________________________ --}}
-
-
-
-
-
-
         <nav id="navbar-presto" class="navbar navbar-expand-lg fixed-top px-4 py-3">
 
             <div class="container-fluid justify-content-between d-flex pe-3">
@@ -22,18 +15,18 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0 px-4 text-center">
                         <li class=" nav-item">
-                            <a class="nav-link d-inline-block px-2 mx-3 pointer" aria-current="page"
+                            <a class="nav-link d-inline-block pointer" aria-current="page"
                                 href="./index.html">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link d-inline-block px-2 mx-3 pointer" href="#">Categorie</a>
+                            <a class="nav-link d-inline-block pointer" href="#">Categorie</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link d-inline-block px-2 mx-3 pointer" href="./articles.html">Tutti gli
+                            <a class="nav-link d-inline-block pointer" href="./articles.html">Tutti gli
                                 annunci</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link d-inline-block px-2 mx-3 pointer" href="#">Profilo</a>
+                            <a class="nav-link d-inline-block pointer" href="#">Profilo</a>
                         </li>
 
                         <li class="nav-item pl-md-0 ml-0 ml-md-4">
@@ -92,73 +85,9 @@
               </form> -->
                 </div>
 
-<<<<<<< HEAD
+
             </div>
 
         </nav>
-=======
-                <li class="nav-item d-flex justify-content-center">
-                    <a class="nav-link d-inline-block px-2 pointer" href="./user_profile.html"><i
-                            class="fas fa-user-circle"></i></a>
-                    <a class="nav-link d-inline-block px-2 pointer" href="./user_profile.html"><i
-                            class="fas fa-shopping-cart"></i></a>
-                </li>
 
-            </ul>
-            <!-- <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form> -->
-        </div>
 
-=======
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Presto.it</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="{{route('login')}}">Accedi</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{route('register')}}">Registrati</a>
-        </li> -->
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          @if(Auth::user())
-          Hello, {{Auth::user()->name}}
-          @else
-          Account
-          @endif
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            @if(Auth::user() == false)
-            <li><a class="dropdown-item" href="{{route('login')}}">Login</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="{{route('register')}}">Register</a></li>
-            
-            @else
-            
-            <li><a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('form-logout').submit();">Logout</a>
-            <form method="POST" action="{{route('logout')}}" id="form-logout">
-              @csrf
-            </form>
-          </li>
-            </ul>
-        </ul>
-        @endif
-      <!-- <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form> -->
->>>>>>> 2e18f6ba8318b1a383dd8c74ba3dcd9d66c74d1a
-    </div>
-
-</nav>
->>>>>>> 2ff2e668c8a64c2bc28a44aae3512e1859b86e78
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,6 +3,7 @@
         Home | Presto.it
     @endpush
 
+    <!-- ======= Header ======= -->
 
     <section id="header" class="d-flex align-items-center">
 
@@ -12,7 +13,7 @@
                     <h1>Vendi il tuo usato o diventa un revisore</h1>
                     <h2>Nuove opportunità e tante offerte</h2>
                     <div>
-                        <a href="#about" class="btn-get-started scrollto">Scopri</a>
+                        <a href="#about" class="btn-get-started">Scopri</a>
                     </div>
                 </div>
                 <div class="col-lg-6 order-1 order-lg-2 hero-img">
@@ -21,6 +22,59 @@
             </div>
         </div>
 
+    </section>
+
+
+    <!-- ======= Categorie ======= -->
+
+    <section id="categories" class="categories section-bg">
+        <div class="container">
+
+            <div class="section-title">
+                <h2>Categorie</h2>
+                <p>Scopri tutte le nostre categorie</p>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                    <div class="icon-box">
+                        <div class="icon"><i class="fas fa-tshirt"></i></div>
+                        <h4 class="title"><a href="">Lorem Ipsum</a></h4>
+                        <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias
+                            excepturi sint occaecati cupiditate</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+                    <div class="icon-box">
+                        <div class="icon"><i class="fas fa-home"></i></div>
+                        <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
+                        <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                            dolore eu fugiat nulla</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box">
+                        <div class="icon"><i class="fas fa-car-side"></i></div>
+                        <h4 class="title"><a href="">Magni Dolores</a></h4>
+                        <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="400">
+                    <div class="icon-box">
+                        <div class="icon"><i class="fas fa-briefcase"></i></div>
+                        <h4 class="title"><a href="">Nemo Enim</a></h4>
+                        <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+                            praesentium voluptatum</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
     </section>
 
     <div class="divprova"></div>
