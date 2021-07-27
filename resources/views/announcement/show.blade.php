84c21c2149b2c237a5dcbbf510ef48fc67f45aae
davidm <davidmalcoci@live.it>
2021-07-27 15:56:50 +0200

    section-show

--- resources/views/announcement/show.blade.php
+++ resources/views/announcement/show.blade.php
@@ -1,15 +1,15 @@
 <x-layout>
 
-
+<section class="mt-5">
     <div class="container">
         <div class="row mt-5">
                 <h1>{{$announcement->title}}</h1>
                 <h3>{{$announcement->description}}</h3>
                 <h3>Categoria: {{$announcement->category->name}}</h3>
                 <h3>Prezzo: {{$announcement->price}}€</h3>
-                <h3>Pubblicato il:{{$announcement->created_at->format('l jS F Y')}}</h3>
+                <h3>Pubblicato il: {{$announcement->created_at->format('l jS F Y')}}</h3>
                 {{-- Aggiungere immagini di placeholder.com con carousell o altro (più dettagli su Trello) --}}
         </div>
     </div>
-
+    </section>
 </x-layout>
