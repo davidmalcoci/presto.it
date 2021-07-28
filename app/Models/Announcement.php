10a01b834503ef9f1d64afd898cb87a4c2c62d6c
Alessandro Cucchiarelli <alessandrosandro1092@gmail.com>
2021-07-28 18:00:42 +0200

    Implementata Dashboard Revisore

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use App\Models\Category;
+use App\Models\Announcement;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -24,6 +26,16 @@ class Announcement extends Model
 
         return $this->belongsTo(Category::class);
 
+    }
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+
+    }
+
+    static public function ToBeRevisionedCount(){
+        return Announcement::where('is_accepted', null)->count();
     }
 
 }
--- resources/views/revisor/home.blade.php
+++ resources/views/revisor/home.blade.php
@@ -5,6 +5,33 @@
     
     <section id="product-show" class="py-5">
         <h1 class="mt-5">Area revisore</h1>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-md-4 d-flex justify-content-center align-items-center offset-md-4 flex-column" style="border: 1px solid black; width: 300px, border-radius: 15px">
+                    
+                    <img src="https://via.placeholder.com/150C/O https://placeholder.com/" alt="">
+                    <h3>{{$announcement->user->id}}</h3>
+                    <h2>{{$announcement->user->name}}</h2>
+                    <h5>{{$announcement->user->email}}</h5>
+                    <h2>{{$announcement->title}}</h2>
+                    <h4>{{$announcement->description}}</h4>
+                    <h5>{{$announcement->created_at->format('l jS F Y')}}</h5>
+
+                    <div class="d-flex justify-content-center">
+                        <i class="fas fa-check"></i>
+                        <i class="fas fa-times"></i>
+
+                    </div>
+
+                </div>
+              
+            </div>
+
+        </div>
+
+        <div>
+ 
+         </div>
        
     </section>
     
